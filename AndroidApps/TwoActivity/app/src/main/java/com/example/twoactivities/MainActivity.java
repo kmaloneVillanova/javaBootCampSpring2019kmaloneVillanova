033625f4e351fc09065a812e015b067c43b7feb9
kmaloneVillanova <kathleen.f.malone@villanova.edu>
2019-04-29 18:38:29 -0400

    TwoActivities

--- AndroidApps/TwoActivity/app/src/main/java/com/example/twoactivities/MainActivity.java
+++ AndroidApps/TwoActivity/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -1,22 +1,43 @@
 package com.example.twoactivities;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    EditText messageText;
+    TextView replyText;
+
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        messageText = findViewById(R.id.messageText);
+        replyText = findViewById(R.id.replyText);
     }
 
     public void launchSecondActivity(View view) {
 
         Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+        String message = messageText.getText().toString();
+        intent.putExtra("message", message);
+        startActivityForResult(intent, 1);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1) {
+            if(resultCode==RESULT_OK) {
+                String reply = data.getStringExtra("reply");
+                replyText.setText(reply);
+            }
+        }
     }
 }
--- AndroidApps/TwoActivity/app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ AndroidApps/TwoActivity/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -1,15 +1,40 @@
 package com.example.twoactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
-    @Override
+    EditText replyText;
+    TextView messageText;
+
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Log.d("SecondActivity", "in SecondActivity onCreate method");
+
+        messageText = findViewById(R.id.messageText);
+        replyText = findViewById(R.id.replyText);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+
+        messageText.setText(message);
+    }
+
+
+    public void launchReply(View view) {
+
+       String reply = replyText.getText().toString();
+       Intent replyIntent = new Intent();
+       replyIntent.putExtra("reply", reply);
+       setResult(RESULT_OK, replyIntent);
+       finish();
     }
 }
